45fe7f3b8503579965255112cc7a7f10c827723f
zeiad-2345 <zeiyadwael@gmail.com>
2025-07-29 18:59:42 +0300

    Update pyp.py

--- pyp.py
+++ pyp.py
@@ -99,6 +99,7 @@ def articles22():
 
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000/")
+
 if __name__ == "__main__":
-    threading.Timer(1.0, open_browser).start() 
-    app.run(debug=True)
+    threading.Timer(1.0, open_browser).start()
+    app.run(debug=True, use_reloader=False) 
